58f5c3d94422c92b24c6d77ce13dd7be1cebe0f6
Matt <madas@hawk.iit.edu>
2021-02-22 10:38:32 -0600

    Initial Commit

---
+++ Shared/Sum123WithErrorPlotApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sum123WithErrorPlotApp.swift
+//  Shared
+//
+//  Created by Matthew Adas on 2/22/21.
+//
+
+import SwiftUI
+
+@main
+struct Sum123WithErrorPlotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
